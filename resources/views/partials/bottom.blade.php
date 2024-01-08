53ba862eeb3d5a9ba483fe1ae716c24925af45ba
unknown <f.carrara24@studenti.unibg.it>
2024-01-08 15:56:43 +0100

    reimplemented all section of the page that were previously held in the same database, in 2 different database (the first based on mamp, the second being pageCategories, static and accessed using config(pageCategoriws.section)

--- resources/views/partials/bottom.blade.php
+++ resources/views/partials/bottom.blade.php
@@ -2,7 +2,17 @@
     <div class="container d-flex flex-row align-items-center">
         <div class="left-part w-50 py-3 pe-5">
             <div class= "d-flex flex-row justify-content-between mb-5">
-
+                {{-- getting data from a config file --}}
+                @foreach (config('pageCategories.footer-categories') as $category)
+                    <div class="d-flex flex-column">
+                        <h3>{{ $category['title'] }}</h2>
+                            <div class="d-flex flex-column " style="color: grey">
+                                @foreach ($category['link'] as $link)
+                                    <div>{{ $link }}</div>
+                                @endforeach
+                            </div>
+                    </div>
+                @endforeach
             </div>
             <span style="color: grey">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat, molestias
                 assumenda fugiat quos,
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,10 +1,18 @@
 <div id="containerHeader">
     <div class=" d-flex flex-row justify-content-evenly container py-2 ">
         <div class="subcontainer-header d-flex flex-row justify-content-start align-items-center w-max-content">
-            <a href="{{route('comics.index')}}"><img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="jumbotron"></a>
+            <a href="{{ route('comics.index') }}"><img src="{{ Vite::asset('resources/img/dc-logo.png') }}"
+                    alt="jumbotron"></a>
         </div>
         <div class="d-flex flex-row justify-content-between gap-5 ">
-
+            @foreach (config('pageCategories.header') as $category)
+                <div class=" d-flex flex-column justify-content-between ">
+                    <div></div>
+                    <div>{{ $category['title'] }}</div>
+                    <div class="bottom-color " style="height: 3px;" class="">
+                    </div>
+                </div>
+            @endforeach
         </div>
         <div class="d-flex flex-column justify-content-center align-items-end ">
             <div class="d-flex flex-row justify-content-end align-content-center">
